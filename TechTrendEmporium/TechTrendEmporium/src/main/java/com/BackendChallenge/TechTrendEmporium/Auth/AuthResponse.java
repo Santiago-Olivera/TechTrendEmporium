5f6ac0fd1847ed410f31166feee31cfcc224d9b1
juromerop <juromerop@unal.edu.co>
2024-03-07 15:35:23 -0500

    feature-jwtcreation: jwt created when sign up new shopper
    The sign up and login for shoppers its working good, with endpoints /api/auth for sign up and /api/login for login, the token conatins the username
    
    to do: include Role in the token and do the login validation with the email, not with the username like is actually working

---
+++ TechTrendEmporium/TechTrendEmporium/src/main/java/com/BackendChallenge/TechTrendEmporium/Auth/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.BackendChallenge.TechTrendEmporium.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token;
+    String email;
+    String username;
+}
---
+++ TechTrendEmporium/TechTrendEmporium/src/main/java/com/BackendChallenge/TechTrendEmporium/Auth/AuthService.java
@@ -0,0 +1,51 @@
+package com.BackendChallenge.TechTrendEmporium.Auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.BackendChallenge.TechTrendEmporium.entity.User;
+import com.BackendChallenge.TechTrendEmporium.repository.UserRepository;
+import com.BackendChallenge.TechTrendEmporium.JWT.JwtService;
+import com.BackendChallenge.TechTrendEmporium.entity.Role;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode( request.getPassword()))
+                .role(Role.SHOPPER)
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .build();
+    }
+}
