0ff214c9598918e69596a79cc251a8288deac929
Santiago Olivera <123512874+Santiago-Olivera@users.noreply.github.com>
2024-03-09 16:43:39 -0500

    Feature list categories (#14)
    * Creating some entities for the DB
    
    * All entities for the DB
    
    * All entities for the DB
    
    * All entities for the DB and aws connection
    
    * Sign up shopperv1
    
    * List of products and List of categories.
    
    what ? persist List of products and List of categories from fakestore api to our database.
    why ? to show all products and categories.
    
    * Merge into main
    
    ---------
    
    Co-authored-by: juromerop <juromerop@unal.edu.co>

--- TechTrendEmporium/TechTrendEmporium/src/main/java/com/BackendChallenge/TechTrendEmporium/entity/Product.java
+++ TechTrendEmporium/TechTrendEmporium/src/main/java/com/BackendChallenge/TechTrendEmporium/entity/Product.java
@@ -14,16 +14,25 @@ public class Product {
 
     private String title; // title should be a string
 
-    private String price; // price should be a string
+    private double price; // price should be a double
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category; // category_id: connect with the category table
+    @Column(name = "category")
+    private String category; // Treat category as a string
+    // category_id: connect with the category table
 
     private String description; // description should be a string
 
     private String image; // image should be a string
 
     private Integer inventoryId; // Inventory_id: an integer for inventory
-}
 
+    @Embedded
+    private Rating rating; // Rating information
+
+    @Data
+    @Embeddable
+    public static class Rating {
+        private double rate;
+        private int count;
+    }
+}
