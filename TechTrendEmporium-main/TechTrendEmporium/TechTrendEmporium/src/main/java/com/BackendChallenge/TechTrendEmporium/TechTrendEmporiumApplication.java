f969a98b3a1c382e0d842bb1f335511131293dbc
juromerop <juromerop@unal.edu.co>
2024-03-12 10:35:15 -0500

    fix

---
+++ TechTrendEmporium-main/TechTrendEmporium/TechTrendEmporium/src/main/java/com/BackendChallenge/TechTrendEmporium/TechTrendEmporiumApplication.java
@@ -0,0 +1,13 @@
+package com.BackendChallenge.TechTrendEmporium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrendEmporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrendEmporiumApplication.class, args);
+	}
+
+}
